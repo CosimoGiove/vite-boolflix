8806b39a36ad2e50c0f00b591d095f238afc6c09
cosimogiove <cosimo18497@gmail.com>
2023-12-14 15:10:28 +0100

    feat: bandiere italiana e gb importata con il  pack

--- src/HeaderRicerca.vue
+++ src/HeaderRicerca.vue
@@ -6,7 +6,7 @@ export default {
         return {
             RicercaFilm: "",
             ArrayFilm: [],
-            UrlFilm:"https://api.themoviedb.org/3/search/movie?api_key=4b7a12e19ac7dfbc0a77194131ed937f&query="
+            UrlFilm:"https://api.themoviedb.org/3/search/movie?api_key=4b7a12e19ac7dfbc0a77194131ed937f&query=",
         }
     },
     methods: {
@@ -29,8 +29,14 @@ export default {
         <div v-for="ArrayFilms in ArrayFilm.results">
             <h2>titolo : {{ ArrayFilms.title }} </h2>
             <h2>titolo originale : {{ ArrayFilms.original_title }}</h2>
-            <h2> lingua : {{ ArrayFilms.original_language }}</h2>
-            <h2>voto : {{ ArrayFilms.vote_average }}</h2>
+            <h2 v-if="ArrayFilms.original_language === 'en' ">
+                <span class="fi fi-gb"></span>
+            </h2>
+            <h2 v-else-if="ArrayFilms.original_language === 'it' ">
+                <span class="fi fi-it"></span>
+            </h2>
+            <h2 v-else=""> lingua : {{ ArrayFilms.original_language }}</h2>
+            <h2>voto : {{ ArrayFilms.vote_average}}</h2>
         </div>
     </header>
 </template>
