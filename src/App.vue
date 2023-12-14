6e952dd5c09ec0473b2df8ca9fdf82a7c0b02ebf
cosimogiove <cosimo18497@gmail.com>
2023-12-14 14:31:22 +0100

    feat: milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
+import HeaderRicerca from './HeaderRicerca.vue';
 export default{
-  name:"appvue"
+  name:"appvue",
+  components:{
+    HeaderRicerca,
+  }
 }
 </script>
 
 <template>
+  <HeaderRicerca></HeaderRicerca>
+
 </template>
 
 <style scoped>
